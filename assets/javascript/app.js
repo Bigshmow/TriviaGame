758dfddbd631cf06f1acbc403ee23f0d119b8935
Bigshmow <dstewart88@gmail.com>
2019-07-24 15:25:29 -0700

    timer and decrement function added leaving space for stop timer function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,3 +20,50 @@ user clicks start button
         start button appears
             click to restart game (not reload page)
 */
+// global variables
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var timer = 30;
+var intervalId;
+
+// functions
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    function runTimer() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+        }
+        
+    function decrement () {
+        timer--;
+        $("#countDown").html("<h2>Time left to answer: </h2>" + timer);
+    }
+
+    function stopTimer() {
+
+    }
+
+    $("#startGame").on("click",function(){
+        // alert("working");
+        runTimer();
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+// run game
