2a6acd1322685d7f89f374502fd88841f9acbd44
Bigshmow <dstewart88@gmail.com>
2019-07-24 15:01:18 -0700

    psuedo code added to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,22 @@
 // alert("working!");
+/*
+user clicks start button
+    start button hide
+    30 second timer appears
+    countdown by 1 second
+        if timer = 0
+            incorrect ++
+            display timeOut gif
+    Question populates
+        Answers 1-4 populate
+            if correctAnswer clicked
+                correct ++
+                display correct gif
+            if wrongAnswer clicked
+                incorrect ++
+                display wrong gif
+    final question answered
+        show final screen with correct/incorrect/time out answers
+        start button appears
+            click to restart game (not reload page)
+*/
